76ca71340ea0e7bc4e1a272ec02510989a8af0cc
Muhailo Yakyshey <muhailoyakyshev1993@gmail.com>
2023-09-25 15:35:51 +0300

    getItem stage

--- src/store/CustomSqliteStorage.ts
+++ src/store/CustomSqliteStorage.ts
@@ -5,36 +5,78 @@ import { AppState } from "../db/entities/AppState";
 const getRepository = async () => {
   const sqlConnectionService = new SqlConnectionService();
   const con = await sqlConnectionService.configureNativeConnection();
-  return await sqlConnectionService.connection?.getRepository(AppState);
+  return sqlConnectionService.connection?.getRepository(AppState);
+};
+
+const getData = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await getRepository();
+      const data = await res?.find({});
+      if (!data) {
+        throw new Error("Error with GET_DATA");
+      }
+      resolve(JSON.parse(data[0].store));
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
 export class CustomSqliteStorage implements StateStorage {
   async getItem(name: string): Promise<string | null> {
     //add setTimeout for resolving problem with jeepSQLite when rerendering starts
+
     setTimeout(async () => {
-      const value = await new Promise(async (resolve) => {
-        const res = await getRepository();
-        const data = await res?.find({});
-        resolve(data);
-      }).then((res) => console.log(res));
+      try {
+        const result = await getData();
+        console.log("**********GET_ITEM:", result);
+        return result;
+      } catch (error) {
+        console.log("Error with SET_TIMEOUT_IN_GET_ITEM:", error);
+      }
     }, 1000);
   }
+
+  //   setTimeout(async () => {
+  //     try {
+  //       const res = await getRepository();
+  //       const data = await res?.find({});
+  //       if (!data) {
+  //         throw new Error("Error with method GET_ITEM");
+  //       }
+  //       console.log(JSON.parse(data[0].store));
+  //       return JSON.parse(data[0].store);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }, 1000);
+  // }
+
+  // const getData = async () => {
+  //     try {
+  //       const res = await getRepository();
+  //       const data = await res?.find({});
+  //       if (!data) {
+  //         throw new Error("Error with method GET_ITEM");
+  //       }
+  //       console.log(JSON.parse(data[0].store));
+  //       return JSON.parse(data[0].store);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   getData();
+  // }
   async setItem(name: string, value: string): Promise<void> {
     try {
       const res = await getRepository();
       if (res) {
         const dataID = (await res.find({}))[0]?.id;
-        if (dataID) {
-          console.log("dataID: ", dataID);
-          let getItem = await res.findOne({ where: { id: dataID } });
-          console.log("Before getItem:", getItem);
-          if (getItem) {
-            getItem.store = JSON.stringify(value);
-            console.log("AFTER getItem:", getItem);
-            await res.save(getItem);
-          } else {
-            console.log("Error with getItem");
-          }
+        let getItem = await res.findOne({ where: { id: dataID } });
+        if (getItem) {
+          getItem = { ...getItem, store: JSON.stringify(value) };
+          await res.save(getItem);
         } else {
           console.log("******First record*********");
           await res.save({
@@ -43,9 +85,9 @@ export class CustomSqliteStorage implements StateStorage {
           });
         }
       }
-      console.log(await res?.find({}));
+      console.log("********SET_ITEM: ", await res?.find({}));
     } catch (error) {
-      console.log("Error with saving JSON data store", error);
+      console.log("Error with SET_ITEM", error);
     }
   }
 
