793489941dbf6482eba86e318e204a70fecd7ae8
anonymous <anonymous@sad.com>
2023-11-08 19:17:28 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+  esbuild: {
+    minifyIdentifiers: false,
+    keepNames: true,
+  },
+  plugins: [react(), legacy()],
+
+});
