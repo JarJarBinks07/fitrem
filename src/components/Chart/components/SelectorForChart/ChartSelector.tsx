bdeba3aa48779d7bd75a0b7634d9b0894a217eb5
Muhailo Yakyshey <muhailoyakyshev1993@gmail.com>
2023-11-17 23:10:59 +0200

    fixed problem with losing ref on pop

--- src/components/Chart/components/SelectorForChart/ChartSelector.tsx
+++ src/components/Chart/components/SelectorForChart/ChartSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { IonButton, IonButtons, IonContent, IonIcon, IonItem, IonList, IonPopover } from "@ionic/react";
 import { caretDown } from "ionicons/icons";
 
@@ -10,21 +10,33 @@ interface IProps {
 
 const ChartSelector: React.FC<IProps> = ({ setChartInterval }) => {
   const popRef = useRef<HTMLIonPopoverElement>(null);
+  const [popOpen, setPopOpen] = useState(false);
+  const openPop = (e: any) => {
+    popRef.current!.event = e;
+    setPopOpen(true);
+  };
   const intervalName = ["week", "2 weeks", "month", "3 months", "6 months", "12 months"];
 
   return (
     <>
       <IonButtons className="selector__buttons">
-        <IonButton id="popover-button" className="selector__button">
+        <IonButton className="selector__button" onClick={openPop}>
           <IonIcon className="selector__icon" slot="icon-only" icon={caretDown}></IonIcon>
         </IonButton>
       </IonButtons>
-      <IonPopover ref={popRef} trigger="popover-button" side="bottom" alignment="start" dismissOnSelect={true}>
+      <IonPopover ref={popRef} isOpen={popOpen} side="bottom" alignment="start" dismissOnSelect={true}>
         <IonContent>
           <IonList>
             {intervalName.map((item) => (
               <div key={item}>
-                <IonItem button={true} detail={false} onClick={() => setChartInterval(item as ChartType)}>
+                <IonItem
+                  button={true}
+                  detail={false}
+                  onClick={() => {
+                    setChartInterval(item as ChartType);
+                    setPopOpen(false);
+                  }}
+                >
                   {item}
                 </IonItem>
               </div>
